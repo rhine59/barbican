0e409c8e7970908eddd952d63595c0ff64bbb2da
rhine59 <rhine59@gmail.com>
2023-07-16 18:50:51 +0100

    update
    Signed-off-by: rhine59 <rhine59@gmail.com>

--- members/views.py
+++ members/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Members Home</h1>')
+    return render(request, 'members/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Members About</h1>')
+    return render(request, 'members/about.html')
 
